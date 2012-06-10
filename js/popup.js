748ebca3b961177e36a88e3557a9a741460c51dc
Benjamin Guillet <benjamin.guillet@gmail.com>
2012-06-10 10:50:15 -0700

    post to fb

--- js/popup.js
+++ js/popup.js
@@ -5,11 +5,9 @@
     //     sendResponse({message: "success"});
     //   });
 
-
     chrome.tabs.getSelected(null,function(tab) {
         var iframe = document.createElement("iframe");
-        //iframe.setAttribute('src', 'http://localhost:3000/?u=' + tab.url);
-        iframe.setAttribute('src', 'http://localhost:3000/form/index');
+        iframe.setAttribute('src', 'http://blazing-sunset-2499.herokuapp.com/form/?share=' + encodeURIComponent(tab.url));
         iframe.setAttribute('scrolling', 'no');
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('style', 'cursor: pointer;margin: 0; padding: 0; border: none; overflow:hidden; width:200px; height:300px; background-color: translucend;');
